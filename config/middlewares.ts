55fd16ccce4a75f440c3d7e41cf85181a171aa81
Lewis Kawase Kennedy <lhkkennedy@gmail.com>
2025-06-09 02:20:15 +0100

    add middleware config cords

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,19 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      // Whitelist your frontend URL(s) here:
+      origin: [
+        'https://consult-teligence-frontend.vercel.app/',
+        'http://localhost:5173'        // if you also test locally
+      ],
+      methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS','HEAD'],
+      headers: ['Content-Type','Authorization','Origin','Accept'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
