a768a2ab6f695bfad34bd540a4eaa172b29cff0c
Lewis Kawase Kennedy <lhkkennedy@gmail.com>
2025-07-11 10:23:03 +0100

    Link user data with consultant data + align the property and timeline-item schema with UI requirements

--- src/api/consultant/controllers/consultant.ts
+++ src/api/consultant/controllers/consultant.ts
@@ -2,6 +2,31 @@
  * consultant controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::consultant.consultant');
+export default factories.createCoreController('api::consultant.consultant', ({ strapi }) => ({
+  async create(ctx) {
+    const { email, password, ...profileData } = ctx.request.body.data;
+
+    // 1. Create the user (or fetch if already exists)
+    let user = await strapi.query('plugin::users-permissions.user').findOne({ where: { email } });
+    if (!user) {
+      user = await strapi.plugins['users-permissions'].services.user.add({
+        email,
+        password,
+        // add other user fields as needed
+      });
+    }
+
+    // 2. Create the consultant profile, linking to userId
+    const consultant = await strapi.service('api::consultant.consultant').create({
+      data: {
+        ...profileData,
+        userId: user.id,
+      },
+    });
+
+    // 3. Return the consultant profile (and optionally user info)
+    ctx.body = consultant;
+  },
+}));
