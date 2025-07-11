472123161c65d8a88fead1cea3b43d76d8638229
Lewis Kawase Kennedy <lhkkennedy@gmail.com>
2025-07-11 15:16:45 +0100

    remove automation for creating user on new consultant creation

--- src/api/consultant/controllers/consultant.ts
+++ src/api/consultant/controllers/consultant.ts
@@ -4,29 +4,31 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::consultant.consultant', ({ strapi }) => ({
-  async create(ctx) {
-    const { email, password, ...profileData } = ctx.request.body.data;
+export default factories.createCoreController('api::consultant.consultant');
 
-    // 1. Create the user (or fetch if already exists)
-    let user = await strapi.query('plugin::users-permissions.user').findOne({ where: { email } });
-    if (!user) {
-      user = await strapi.plugins['users-permissions'].services.user.add({
-        email,
-        password,
-        // add other user fields as needed
-      });
-    }
+// export default factories.createCoreController('api::consultant.consultant', ({ strapi }) => ({
+//   async create(ctx) {
+//     const { email, password, ...profileData } = ctx.request.body.data;
 
-    // 2. Create the consultant profile, linking to userId
-    const consultant = await strapi.service('api::consultant.consultant').create({
-      data: {
-        ...profileData,
-        userId: user.id,
-      },
-    });
+//     // 1. Create the user (or fetch if already exists)
+//     let user = await strapi.query('plugin::users-permissions.user').findOne({ where: { email } });
+//     if (!user) {
+//       user = await strapi.plugins['users-permissions'].services.user.add({
+//         email,
+//         password,
+//         // add other user fields as needed
+//       });
+//     }
 
-    // 3. Return the consultant profile (and optionally user info)
-    ctx.body = consultant;
-  },
-}));
+//     // 2. Create the consultant profile, linking to userId
+//     const consultant = await strapi.service('api::consultant.consultant').create({
+//       data: {
+//         ...profileData,
+//         userId: user.id,
+//       },
+//     });
+
+//     // 3. Return the consultant profile (and optionally user info)
+//     ctx.body = consultant;
+//   },
+// }));
