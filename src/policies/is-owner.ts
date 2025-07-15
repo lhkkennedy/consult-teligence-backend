82f63e165d083bfecc56cd06a373756100910871
Lewis Kawase Kennedy <lhkkennedy@gmail.com>
2025-07-15 18:43:48 +0100

    suspension of user check for uploading mock timeline items for demo

--- src/policies/is-owner.ts
+++ src/policies/is-owner.ts
@@ -2,9 +2,9 @@ export default async (ctx, next) => {
   const { id } = ctx.params;
   const user = ctx.state.user;
 
-  if (!user) {
-    return ctx.unauthorized('You must be logged in.');
-  }
+  // if (!user) {
+  //   return ctx.unauthorized('You must be logged in.');
+  // }
 
   // Strapi v5: Use documentService to fetch the timeline item with its author
   const item = await strapi.service('api::timeline-item.timeline-item').findOne(id, {
